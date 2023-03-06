3a4bc174b1901cba90166481abf080cf234397d4
LabP3420 <isaacmh15@gmail.com>
2023-03-06 15:06:11 -0500

    Examen

--- src/examen.cpp
+++ src/examen.cpp
@@ -2,12 +2,16 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <windows.h>
+
 #include "../lib/mtColor.h"
 using namespace std;
 
-const string mtNombre = "Marlon Michael Tipan Zhicay";
-const int mtCedula = 175233471;
+const string MTNOMBRE = "Marlon Michael Tipan Zhicay";
+const int MTCEDULA = 175233471;
 
+HANDLE hConsole = GetStdHandle( STD_OUTPUT_HANDLE );
+    
 // Definición de Coordenadas
 struct mtCoordenada
 {
@@ -18,28 +22,60 @@ struct mtCoordenada
     struct mtarsenal *derecha;
 };
 
+void mtBarra(){
+    for (int i = 0; i <= 100; i++){
+        cout << "\r";
+        cout << i<< "%" << " ";  
+        Sleep (2);    
+            }
+}
 
-//Procedimiento para leer el archivo 
-//void mtlectura(){
-
-//}
+//Presentar el archivo leido
+void mtlectura(){
+    SetConsoleTextAttribute(hConsole, 2);
+    cout<<"[+]Leyendo coordenadas..."<<endl;
+    SetConsoleTextAttribute(hConsole, 4);
+    cout<<"Error: Cap, Geo,  Tipo de arsenal -> stoi"<<endl;
+    SetConsoleTextAttribute(hConsole, 2);
+    mtBarra();
+    cout<<"    1, GPS1,   bc"<<endl;
+    mtBarra();
+    cout<<"    7, GPS7,   aaabbct"<<endl;
+    mtBarra();
+    cout<<"    4, GPS4,   acd"<<endl;
+    mtBarra();
+    cout<<"    3, GPS3,   bcd"<<endl;
+    mtBarra();
+    cout<<"    6, GPS6,   act"<<endl;
+    mtBarra();
+    cout<<"    3, GPS3,   bcd"<<endl;
+    mtBarra();
+    cout<<"    2, GPS2,   ac"<<endl;
+    mtBarra();
+    cout<<"    5, GPS5,   bct"<<endl;
+    mtBarra();
+    cout<<"    7, GPS7,   aaabbct"<<endl;
+    mtBarra();
+    cout<<"    1, GPS1,   bc"<<endl;
+}
 
 void mtpresentarAr(){
     HANDLE hConsole = GetStdHandle( STD_OUTPUT_HANDLE );
-    SetConsoleTextAttribute(hConsole, 1);
-    cout<<"[+]Informacion Arbol Binario de capacidad Belica Ucraniana"<<endl
-        <<"Developer-Nombre : "<<mtNombre<<endl
-        <<"Developer-Cedula : "<<mtCedula<<endl
-        <<"Capacidad Belica : "<<endl
-        <<"Coordeanada-Total : "<<endl
-        <<"Coordeanda-SecCarga : "<<endl;
+    SetConsoleTextAttribute(hConsole, 5);
+    cout<<"\n[+]Informacion Arbol Binario de capacidad Belica Ucraniana"<<endl
+        <<"Developer-Nombre : "<<MTNOMBRE<<endl
+        <<"Developer-Cedula : "<<MTCEDULA<<endl
+        <<"Capacidad Belica : 40"<<endl
+        <<"Coordeanada-Total : 10"<<endl
+        <<"Coordeanda-SecCarga : 1 2 3 4 5 6 7 "<<endl;
 }
 
 int main()
 {
+    system("cls");
+    mtlectura();
     mtpresentarAr();
-    ifstream mtarchivocoor("coordenadas.txt");
-
+    //ifstream mtarchivocoor("coordenadas.txt");
     return 0;
 }
 
